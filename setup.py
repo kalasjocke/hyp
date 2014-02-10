34c9b15775ae7da3a613ef38f56235bbe0181f9a
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-02-10 21:53:08 +0100

    Initial package structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Hy',
+    version='0.0.1',
+    packages=['hy'],
+    license='MIT',
+    url='https://github.com/kalasjocke/hy',
+    long_description=open('README.md').read(),
+)
