06dd856ce57193f34395f8ee6e7c7d3030356609
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-02-11 10:16:40 +0100

    Add test for multiple resources

--- tests/test_single.py
+++ tests/test_single.py
@@ -9,6 +9,20 @@ def test_single(post):
     assert json.loads(data) == {'posts': [{'id': 1, 'title': 'My title'}]}
 
 
+def test_multiple(post_factory):
+    post = post_factory(id=1, title='A title')
+    another_post = post_factory(id=2, title='Another title')
+
+    data = PostSerializer().to_json([post, another_post])
+
+    assert json.loads(data) == {
+        'posts': [
+            {'id': 1, 'title': 'A title'},
+            {'id': 2, 'title': 'Another title'},
+        ]
+    }
+
+
 def test_meta(post):
     data = PostSerializer().to_json(post, meta={'key': 'value'})
 
