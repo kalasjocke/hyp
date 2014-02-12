cb7226bbf5080e8a742f5262242a26e0a73ffd15
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-02-12 09:42:13 +0100

    Add author to PyPI package

--- setup.py
+++ setup.py
@@ -5,6 +5,8 @@ setup(
     version='0.0.1',
     packages=['hy'],
     license='MIT',
+    author='Joakim Ekberg',
+    author_email='jocke.ekberg@gmail.com',
     url='https://github.com/kalasjocke/hy',
     long_description=open('README.md').read(),
 )
