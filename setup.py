39cdae2d41553fe0369d6cd7bb9e05154ecde107
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-02-12 14:44:30 +0100

    Add PyPI package description

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     author='Joakim Ekberg',
     author_email='jocke.ekberg@gmail.com',
     url='https://github.com/kalasjocke/hyp',
+    description='Partial JSON API implementation in Python on top of Schematics',
     long_description=open('README.md').read(),
     install_requires=open('requirements.txt').read().split(),
 )
