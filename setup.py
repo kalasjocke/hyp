deef3783419d1e5c1f680cbcac945017d45f1b69
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-04-24 14:24:56 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='hyp',
-    version='0.3.0',
+    version='0.3.1',
     packages=['hyp', 'hyp.adapters'],
     license='MIT',
     author='Joakim Ekberg',
