f299e85b2a2fb2e07528a6c33abe3d0306bad5cc
Steven Loria <sloria1@gmail.com>
2014-10-22 19:39:02 -0400

    Import Schema instead of Serializer from marshmallow
    Also get rid of unnecessary serializer instantiatiions in
    adapter_for

--- hyp/adapters/base.py
+++ hyp/adapters/base.py
@@ -1,16 +1,16 @@
 from __future__ import absolute_import
 
 from schematics.models import Model as SchematicsSerializer
-from marshmallow import Serializer as MarshmallowSerializer
+from marshmallow import Schema as MarshmallowSerializer
 
 from hyp.adapters.schematics import Adapter as SchematicsAdapter
 from hyp.adapters.marshmallow import Adapter as MarshmallowAdapter
 
 
 def adapter_for(serializer):
-    if isinstance(serializer(), SchematicsSerializer):
+    if issubclass(serializer, SchematicsSerializer):
         return SchematicsAdapter
-    elif isinstance(serializer(), MarshmallowSerializer):
+    elif issubclass(serializer, MarshmallowSerializer):
         return MarshmallowAdapter
     else:
         raise NotImplementedError()
